3deffb95f178f237854b2a0b2331db29d0cdd3f6
Yogeth <yogethcr@gmail.com>
2025-12-11 13:46:52 +0530

    update StudentController.java

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 import java.util.*;
+import com.examaple.demo.model.Student;
 import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
